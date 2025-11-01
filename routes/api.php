9d64bf3e5ef4132cbab1a9c06d2f27aa2c0306ac
Kassem Yahia <kassemhajyahia@gmail.com>
2025-11-01 15:05:05 +0300

    database to sqlite

--- routes/api.php
+++ routes/api.php
@@ -2,23 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 
-Route::post('/login', [AuthController::class, 'login']);
-
-//Route::post('/login', function (Request $request) {
-//    return response()->json([
-//        'message' => 'تم تسجيل الدخول بنجاح ✅',
-//        'email' => $request->email,
-//        'password' => $request->password
-//    ]);
-//});
-
-
-
-Route::get('/test', function () {
-    return response()->json([
-        'message' => 'API working successfully 🎉',
-        'status' => 'ok'
-    ]);
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
 });
