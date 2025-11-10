ed642cbefb87ed31cb1286d928e5f4984a93678e
Kassem Yahia <kassemhajyahia@gmail.com>
2025-11-10 12:04:49 +0300

    utf8

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,6 @@ class AppServiceProvider extends ServiceProvider
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
-        \Illuminate\Support\Facades\DB::statement("SET NAMES 'utf8'");
 
     }
 }
